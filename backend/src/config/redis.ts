9ee099d3c52b367a9a0cf84d38e35d094544364e
snehithha <mulugusnehithha@gmail.com>
2026-07-29 08:57:28 +0530

    redis database

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -3,9 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const redis = new IORedis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
+const redis = new IORedis(process.env.REDIS_URL!, {
   maxRetriesPerRequest: null,
 });
 
