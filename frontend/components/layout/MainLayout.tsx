109990fd349dcb7f1e484af384fd9eb1386657dc
snehithha <mulugusnehithha@gmail.com>
2026-07-29 09:39:10 +0530

    Vercel deployment

--- frontend/components/layout/MainLayout.tsx
+++ frontend/components/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 
@@ -9,14 +9,13 @@ interface Props {
 export default function MainLayout({ children }: Props) {
   return (
     <div className="flex h-screen w-screen overflow-hidden bg-white">
-      {/* Sidebar */}
-      <Sidebar />
+      <Suspense fallback={null}>
+        <Sidebar />
+      </Suspense>
 
-      {/* Right Section */}
       <div className="flex flex-1 flex-col overflow-hidden">
         <Header />
 
-        {/* Content */}
         <main className="flex flex-1 overflow-hidden bg-white">
           {children}
         </main>
--- frontend/services/api.tsx
+++ frontend/services/api.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL + "/api",
   headers: {
     "Content-Type": "application/json",
   },
